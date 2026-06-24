614b48584a45fa22107c592b9d959775c533c909
Samridh Palleda <psamridh007@gmail.com>
2026-06-24 19:43:36 +0530

    auth and db sync

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest";
 import dotenv from "dotenv/config";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// ! middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true, // ? server allows browser to include cookies in request
+  }),
+);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "api is up and running" });
@@ -17,5 +30,3 @@ connectDB()
     });
   })
   .catch((err) => console.log("💥Error starting the server:", err));
-
-
