f31bbad070b2f6ab0d80842d4d7da62155a0746f
Samridh Palleda <psamridh007@gmail.com>
2026-06-25 19:11:29 +0530

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,14 +1,16 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.models.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "algo-meet" });
 
-// function to add user in MongoDB
+// function to add user in MongoDB and Stream
 const syncUser = inngest.createFunction(
   { id: "sync-user", triggers: { event: "clerk/user.created" } },
   async ({ event }) => {
+    // ! saving user to MongoDB
     await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } =
       event.data;
@@ -22,21 +24,28 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    // todo: dp something else
+    // ! saving user to Stream
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   },
 );
 
-// function to delete user from MongoDB
+// function to delete user from MongoDB and Stream
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db", triggers: { event: "clerk/user.deleted" } },
-
+  
   async ({ event }) => {
+    // ! deleting user from MongoDB
     await connectDB();
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
 
-    // todo: dp something else
+    // ! deleting user from Stream
+    await deleteStreamUser(id.toString());
   },
 );
 
