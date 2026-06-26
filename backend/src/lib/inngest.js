efd1c35bb118870af0a9a0483fa8501da28099ff
Samridh Palleda <psamridh007@gmail.com>
2026-06-26 20:13:46 +0530

    todo challenge added

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -30,6 +30,8 @@ const syncUser = inngest.createFunction(
       name: newUser.name,
       image: newUser.profileImage,
     });
+
+    // todo: challenge -> send a welcome email to user
   },
 );
 
